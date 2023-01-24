50d2efbbb72d25e32bd0169e31e34fb67711fe9f
woicip <woicipta@gmail.com>
2023-01-24 23:18:11 +0700

    update: SenderMessage

--- src/components/Message/Detail/SenderMessage.vue
+++ src/components/Message/Detail/SenderMessage.vue
@@ -9,8 +9,7 @@
     <div class="text-left">
         <div class="flex items-center">
             <h1 class="text-[15px] font-bold">{{ props.sender }}</h1>
-            <div class="mx-[10px] w-[3px] h-[3px] bg-white/40 rounded-full"></div>
-            <p class="text-[12px] text-white/40">{{ props.hoursMinutes }}</p>
+            <!-- <div class="mx-[10px] w-[3px] h-[3px] bg-white/40 rounded-full"></div> -->
         </div>
 
         <p v-bind:innerHTML="props.message" class="mt-[5px] text-[15px] text-white/80"></p>
