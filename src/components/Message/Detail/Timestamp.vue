6a827a19df4db218e4f750b4b0094cdd6b2a521b
woicip <woicipta@gmail.com>
2023-01-24 23:18:18 +0700

    update: Timestamp

--- src/components/Message/Detail/Timestamp.vue
+++ src/components/Message/Detail/Timestamp.vue
@@ -1,13 +1,13 @@
 <script setup>
     const props = defineProps({
         dateTimestamp: String,
-        commentLength: Number
+        hoursMinutes: String
     })
 </script>
 <template>
     <div class="mt-[35px] flex items-center">
-        <p class="text-[12px] text-white/40">{{ props.dateTimestamp }}</p>
+        <p class="text-[12px] text-white/40">{{ props.hoursMinutes }}</p>
         <div class="mx-[10px] w-[3px] h-[3px] bg-white/40 rounded-full"></div>
-        <p class="text-[12px] text-white/40">{{ props.commentLength }} Comments</p>
+        <p class="text-[12px] text-white/40">{{ props.dateTimestamp }}</p>
     </div>
 </template>
