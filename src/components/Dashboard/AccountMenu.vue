2a0df156ca12e46cdd8e0a9521795992c6701d98
woicip <woicipta@gmail.com>
2023-01-26 01:58:58 +0700

    update: UI naming

--- src/components/Dashboard/AccountMenu.vue
+++ src/components/Dashboard/AccountMenu.vue
@@ -10,7 +10,7 @@
 </script>
 <template>
     <div class="ml-[8px] relative bottom-[-1px]">
-        <button v-if="accountView" class="py-[10px] px-[15px] text-sm font-semibold mobileL:text-[13px] rounded-t-lg bg-indigo-500 border-b border-indigo-500">Account</button>
-        <button v-else class="py-[10px] px-[15px] text-sm font-semibold mobileL:text-[13px] rounded-t-md hover:bg-white/10 border-b border-transparent hover:border-white" @click="accountViewHandler">Account</button>
+        <button v-if="accountView" class="py-[10px] px-[15px] text-sm font-semibold mobileL:text-[13px] rounded-t-lg bg-indigo-500 border-b border-indigo-500">Account Center</button>
+        <button v-else class="py-[10px] px-[15px] text-sm font-semibold mobileL:text-[13px] rounded-t-md hover:bg-white/10 border-b border-transparent hover:border-white" @click="accountViewHandler">Account Center</button>
     </div>
 </template>
