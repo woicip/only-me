ac3072b236302165b92ae662eafa51031944bbeb
woicip <woicipta@gmail.com>
2023-01-26 01:59:09 +0700

    update: Nav

--- src/components/Dashboard/Nav.vue
+++ src/components/Dashboard/Nav.vue
@@ -1,14 +1,5 @@
-<script setup>
-    function logoutHandler(){
-        localStorage.removeItem('onl_auth');
-        router.push('/');
-    }
-</script>
 <template>
     <nav class="flex items-center justify-between pb-[20px] mobileL:pb-[15px] mobileL:px-[15px] border-b border-white/10">
         <a href="/dashboard" class="text-lg mobileL:text-[15px] font-bold">OnlyMe <span class="py-[2px] px-[5px] ml-[6px] text-xs rounded-md bg-indigo-500">Dashboard</span></a>
-        <div>
-            <button class="text-[14px] text-white/40 hover:text-red-500 hover:bg-red-500/10 transition-all py-[3px] px-[12px] rounded-full mobileL:text-[13px]" @click="logoutHandler">Log out</button>
-        </div>
     </nav>
 </template>
