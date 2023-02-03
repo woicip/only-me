e08d0612742bb32b604818ad7d4953f94f581b47
woicip <woicipta@gmail.com>
2023-02-03 17:26:09 +0700

    update: GetVerified dashboard

--- src/components/Dashboard/Account/GetVerified.vue
+++ src/components/Dashboard/Account/GetVerified.vue
@@ -1,28 +1,43 @@
 <template>
-    <div class="py-[20px] pb-[10px] px-[20px] animate-fadeIn">
+    <div class="py-[20px] pb-[10px] px-[20px] mobileL:px-[15px] animate-fadeIn">
         <div>
-            <h1 class="text-[18px] font-medium flex items-center">
+            <h1 class="text-[18px] mobileL:text-[17px] font-medium flex items-center">
                 Get Your Verified Badge!
                 <img src="https://img.icons8.com/color/48/000000/verified-badge.png" class="w-[25px] ml-[6px]"/>
             </h1>
-            <p class="mt-[5px] text-white/80 text-[15px]">There are several information that I want to share to you before you get the verified badge.</p>
+            <p class="mt-[5px] text-white/80 text-[15px] mobileL:text-[14px]">There are several information that I want to share to you before you get the verified badge.</p>
 
             <div class="mt-[40px]">
-                <h2 class="font-medium text-[18px]">Why you should get verified ?</h2>
+                <h2 class="font-medium text-[18px] mobileL:text-[17px]">Why you should get verified ?</h2>
                 <ul class="mt-[5px]">
-                    <li class="py-[3px] text-white/80 text-[15px] flex items-center">
+                    <li class="py-[3px] text-white/80 text-[15px] mobileL:text-[14px] flex items-center">
                         <img src="https://img.icons8.com/color/48/000000/verified-badge.png" class="w-[22px] mr-[10px]"/>
                         Prevent someone to impersonate you
                     </li>
-                    <li class="py-[3px] text-white/80 text-[15px] flex items-center">
+                    <li class="py-[3px] text-white/80 text-[15px] mobileL:text-[14px] flex items-center">
                         <img src="https://img.icons8.com/color/48/000000/verified-badge.png" class="w-[22px] mr-[10px]"/>
                         Make sure someone sending a message to the right person
                     </li>
-                    <li class="py-[3px] text-white/80 text-[15px] flex items-center">
+                    <li class="py-[3px] text-white/80 text-[15px] mobileL:text-[14px] flex items-center">
                         <img src="https://img.icons8.com/color/48/000000/verified-badge.png" class="w-[22px] mr-[10px]"/>
                         Also make sure your account is not a bot
                     </li>
                 </ul>
+            </div>
+
+            <div class="mt-[40px]">
+                <h1 class="font-medium text-[18px] mobileL:text-[17px]">Requirements</h1>
+                <div>
+                    <div class="mt-[10px] flex items-center">
+                        <div class="w-[20px] h-[20px] mr-[10px] bg-white/10 rounded-md"></div>
+                        <h1 class="text-white/80 text-[15px] mobileL:text-[14px]">1000 Messages</h1>
+                    </div>
+
+                    <div class="mt-[10px] flex items-center">
+                        <div class="w-[20px] h-[20px] mr-[10px] bg-white/10 rounded-md"></div>
+                        <h1 class="text-white/80 text-[15px] mobileL:text-[14px]">500 Comments</h1>
+                    </div>
+                </div>
             </div>
 
             <button class="w-full py-[10px] mt-[40px] bg-blue-500/90 font-medium rounded-lg hover:bg-blue-500/80">Get Verified</button>
