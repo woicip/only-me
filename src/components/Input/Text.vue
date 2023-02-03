d3ec295063f4837ce96c537ee66317e641ad0b9e
woicip <woicipta@gmail.com>
2023-02-03 17:27:32 +0700

    update: Text

--- src/components/Input/Text.vue
+++ src/components/Input/Text.vue
@@ -16,7 +16,7 @@
 
 <template>
     <div class="w-full py-[10px] px-[15px] bg-white/5 rounded-[8px] ring-1 ring-white/10">
-        <p class="text-[11px] text-white/40 font-light">{{ name }}</p>
+        <p class="text-[11px] text-white/70">{{ name }}</p>
         <input :value="value" type="text" :placeholder="placeholder" class="w-full text-sm text-white bg-transparent focus:outline-none placeholder:text-sm placeholder:text-white/20" @input="inputHandler">
     </div>
 </template>
