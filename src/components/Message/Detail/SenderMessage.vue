e0b63360d28d2c18ff3c4889d728dbd69f07c09c
woicip <woicipta@gmail.com>
2023-02-03 17:28:05 +0700

    update: SenderMessage

--- src/components/Message/Detail/SenderMessage.vue
+++ src/components/Message/Detail/SenderMessage.vue
@@ -1,17 +1,21 @@
 <script setup>
+    import parseText from '../../../functions/parseText';
+
     const props = defineProps({
         sender: String,
         message: String,
         hoursMinutes: String
     })
+
+    const message = parseText(props.message);
 </script>
 <template>
     <div class="text-left">
         <div class="flex items-center">
-            <h1 class="text-[15px] font-bold">{{ props.sender }}</h1>
+            <h1 class="text-[15px] mobileL:text-[14px] font-bold">{{ props.sender }}</h1>
             <!-- <div class="mx-[10px] w-[3px] h-[3px] bg-white/40 rounded-full"></div> -->
         </div>
 
-        <p v-bind:innerHTML="props.message" class="mt-[5px] text-[15px] text-white/80"></p>
+        <div v-bind:innerHTML="message" class="mt-[5px] text-[15px] mobileL:text-[14px] text-white/80"></div>
     </div>
 </template>
