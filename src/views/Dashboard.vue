ae0801c6606d2152fa3a5e221df52f0fa67d850f
woicip <woicipta@gmail.com>
2023-02-03 17:27:21 +0700

    update: Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -13,15 +13,9 @@
     import Profile from '../components/Dashboard/Profile.vue';
     import Account from '../components/Dashboard/Account.vue';
     import Nav from '../components/Dashboard/Nav.vue';
-    import parseJWT from '../functions/parseJWT';
-    import parseURL from '../functions/parseURL';
-    import parseNL from '../functions/parseNL';
 
     // graphql
     import graphql from '../fetchs/graphql';
-    import updateUserFullname from '../../graphql/mutation/updateFullname';
-    import updateUsernameQuery from '../../graphql/mutation/updateUsername';
-    import updateBioQuery from '../../graphql/mutation/updateBio';
     import checkToken from '../../graphql/query/ct';
 
     const authUser = useAuthStore();
@@ -31,14 +25,7 @@
     const profileView = ref(false);
     const accountView = ref(false);
 
-    const bio = ref("");
-    const username = ref("");
-    const fullname = ref("");
-    const comment = ref("");
-    const author = ref(true);
     const updated = ref(false);
-
-    const parsed = parseJWT(localStorage.getItem('onl_auth'));
 
     function messageViewHandler(){
         messageView.value = true;
@@ -58,96 +45,8 @@
         accountView.value = true;
     }
 
-    function usernameInput(e){
-        username.value = e.target.value;
-    }
-
-    function fullnameInput(e){
-        fullname.value = e.target.value;
-    }
-
-    function bioInput(e){
-        bio.value = e.target.value;
-    }
-
     function updatedHandler(){
         updated.value = false;
-    }
-
-    function avatarChange(e){
-        const files = e.target.files;
-        const selected = files[0];
-        console.log(selected);
-
-        if(!selected.type.includes("image")){
-            alert("Avatar only support image file.");
-        }
-
-        const avatar = URL.createObjectURL(selected);
-        console.log(avatar);
-    }
-
-    async function updateFullname(){
-        const authToken = localStorage.getItem('onl_auth');
-        const { data: { updateFullname } } = await graphql({ query: updateUserFullname(authToken, fullname.value)});
-        if(updateFullname.status === "OK"){
-            const user = await FetchDashboardProfile();
-    
-            updated.value = true;
-            fullname.value = "";
-            dashboardProfile.fullname.value = user.fullname;
-    
-            setTimeout(() => {
-                updated.value = false;
-            }, 3000);
-
-        } else {
-            alert("Failed update fullname");
-        }
-    }
-
-    async function updateUsername(){
-        const authToken = localStorage.getItem('onl_auth');
-        const { data: { updateUsername } } = await graphql({ query: updateUsernameQuery(authToken, username.value) });
-
-        if(updateUsername.status === "OK"){
-            const user = await FetchDashboardProfile();
-
-            updated.value = true;
-            username.value = "";
-            authUser.usernameHandler(user.username);
-            dashboardProfile.username.value = user.username;
-            document.title = `${user.username} - OnlyMe`;
-    
-            setTimeout(() => {
-                updated.value = false;
-            }, 3000);
-
-        } else {
-            alert("Failed update username");
-        }
-    }
-
-    async function updateBio(){
-        const parseBioURL = parseURL(bio.value);
-        const parseBioNL = parseNL(parseBioURL);
-        const authToken = localStorage.getItem('onl_auth');
-
-        const { data: { updateBio } } =  await graphql({ query: updateBioQuery(authToken, parseBioNL) });
-        if(updateBio.status === "OK"){
-            const user = await FetchDashboardProfile();
-    
-            updated.value = true;
-            bio.value = "";
-            dashboardProfile.bio.value = user.bio;
-    
-            setTimeout(() => {
-                updated.value = false;
-            }, 3000);
-
-        } else {
-            alert("Failed update bio");
-        }
     }
 
     onMounted(async () => {
@@ -173,36 +72,26 @@
     <section>
         <Updated :updated="updated" :updatedHandler="updatedHandler" />
 
-        <section class="w-[536px] mobileL:w-screen mx-auto py-[50px] mobileL:py-[15px] text-white">
+        <section class="w-[536px] tabletL:w-full mx-auto  text-white">
             <Nav />
 
-            <section class="mobileL:px-[15px]">
+            <section class="mobileL:px-[15px] tabletL:px-[30px] tabletM:px-[20px]">
                 <HeaderProfile />
             </section>
 
-
             <!-- Buttons Navigation -->
-            <div class="mt-[50px] mobileL:mt-[50px] mobileL:px-[15px] relative flex items-center border-b border-white/10 mobileL:sticky">
+            <div class="mt-[50px] pt-[15px] mobileL:mt-[50px] mobileL:px-[15px] tabletL:px-[30px] tabletM:px-[20px] flex items-center border-b border-white/20 sticky top-[0px] bg-[#373A44] z-10">
                 <MessageMenu :messageView="messageView" :handler="messageViewHandler" />
                 <ProfileMenu :profileView="profileView" :handler="profileViewHandler" />
                 <AccountMenu :accountView="accountView" :handler="accountViewHandler" />
             </div>
 
             <!-- Content Navigation -->
-            <section class="mt-[20px]">
+            <section class="mt-[20px] pb-[30px]">
                 <Messages v-if="messageView" />
 
                 <!-- Profile Content View -->
-                <Profile v-if="profileView"
-                    :id="parsed.id"
-                    :avatarChange="avatarChange"
-                    :fullnameInput="fullnameInput"
-                    :updateFullname="updateFullname"
-                    :usernameInput="usernameInput"
-                    :updateUsername="updateUsername"
-                    :bioInput="bioInput"
-                    :updateBio="updateBio"
-                />
+                <Profile v-if="profileView" />
 
                 <!-- Account Section -->
                 <Account v-if="accountView"  />
