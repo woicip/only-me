2e6edc171fc4757bc1a20abbaf16cdbd8994682c
woicip <woicipta@gmail.com>
2023-02-03 17:25:44 +0700

    update: Nav dashboard

--- src/components/Dashboard/Nav.vue
+++ src/components/Dashboard/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex items-center justify-between pb-[20px] mobileL:pb-[15px] mobileL:px-[15px] border-b border-white/10">
+    <nav class="flex items-center justify-between py-[20px] mobileL:py-[15px] mobileL:px-[15px] tabletL:px-[30px] tabletM:px-[20px] border-b border-white/10 bg-[#373A44]">
         <a href="/dashboard" class="text-lg mobileL:text-[15px] font-bold">OnlyMe <span class="py-[2px] px-[5px] ml-[6px] text-xs rounded-md bg-indigo-500">Dashboard</span></a>
     </nav>
 </template>
