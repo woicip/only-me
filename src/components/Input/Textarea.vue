99d5ca414908b1bb58a23db960086906bb4a3fed
woicip <woicipta@gmail.com>
2023-02-03 17:27:37 +0700

    update: Textarea

--- src/components/Input/Textarea.vue
+++ src/components/Input/Textarea.vue
@@ -16,7 +16,7 @@
 
 <template>
     <div class="mt-[10px] py-[10px] px-[15px] bg-white/5 rounded-[8px] ring-1 ring-white/10">
-        <p class="text-[11px] text-white/40 font-light">{{ name }}</p>
+        <p class="text-[11px] text-white/70">{{ name }}</p>
         <textarea :value="value" name="message" id="message" :placeholder="placeholder" class="w-full h-[96px] text-sm text-white bg-transparent focus:outline-none placeholder:text-sm placeholder:text-white/20" @input="inputHandler"></textarea>
     </div>
 </template>
